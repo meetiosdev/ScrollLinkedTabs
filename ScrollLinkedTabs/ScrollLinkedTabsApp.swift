1309cf51a9ea1488d472bcfc0adcb13e35624681
Swarajmeet Singh <swarajmeet.crebos@gmail.com>
2025-06-26 18:14:22 +0530

    Initial Commit

---
+++ ScrollLinkedTabs/ScrollLinkedTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollLinkedTabsApp.swift
+//  ScrollLinkedTabs
+//
+//  Created by Swarajmeet Singh on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrollLinkedTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
