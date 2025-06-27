0570a018843cdc2af8c69b43902d2eb9ef26dd9b
Swarajmeet Singh <swarajmeet.crebos@gmail.com>
2025-06-27 11:18:09 +0530

    TabsBarView added

---
+++ ScrollLinkedTabs/Views/TabsBarView.swift
@@ -0,0 +1,67 @@
+
+import SwiftUI
+
+// MARK: - CommunityTabsView
+
+/// A horizontally scrollable, animated tab bar for selecting communities.
+///
+/// This view fetches community data via `CommunityViewModel` and renders each as an interactive tab item.
+/// It handles infinite scrolling and scroll-to-selected behavior.
+struct TabsBarView: View {
+
+    // MARK: - State & Dependencies
+
+    @State private var viewModel: HomeViewModel
+    @Namespace private var animation
+
+    init(viewModel: HomeViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    // MARK: - Body
+
+    var body: some View {
+        ScrollViewReader { scrollProxy in
+            ScrollView(.horizontal, showsIndicators: false) {
+                if viewModel.topics.isEmpty && viewModel.isLoading {
+                    loadingShimmer(count: 6)
+                } else {
+                    LazyHStack(spacing: 24) {
+                        ForEach(viewModel.topics, id: \ .id) { topic in
+                            TabItemView(
+                                topic: topic,
+                                isActive: viewModel.isTopicSelected(topic),
+                                animationNamespace: animation
+                            ) {
+                                viewModel.selectTopic(topic)
+                            }
+                            .id(topic.id)
+                        }
+                    }
+                   // .padding(.horizontal, 16)
+                   // .redacted(reason: viewModel.communities.isEmpty ? .placeholder : [])
+                }
+               
+            }
+            .contentMargins(.horizontal, 16)
+            .onChange(of: viewModel.selectedTopic) { oldValue, topic in
+                guard let topic = topic else { return }
+                withAnimation {
+                    scrollProxy.scrollTo(topic.id, anchor: .center)
+                }
+            }
+        }
+    }
+
+    
+    private func loadingShimmer(count: Int) -> some View {
+        LazyHStack(spacing: 24) {
+            ForEach(0..<count, id: \.self) { index in
+                Color.gray
+                    .frame(width: CGFloat.random(in: 80...120), height: 20)
+                    .clipShape(.rect(cornerRadius: 4))
+                    .offset(y: -3)
+            }
+        }
+    }
+}
